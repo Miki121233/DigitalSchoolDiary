90ca5f66e2019e482c12863a31fe341a623756ef
MPR21 <mikolaj.PLADER.intern@3ds.com>
2023-10-16 15:11:59 +0200

    Bootstrap, font-awesome added, Simple HTTPS requests from client to API works

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+
+export class AppComponent implements OnInit {
+  title = 'DatingApp';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    console.log('próba');
+    
+    this.http.get('https://localhost:5002/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+
 }
