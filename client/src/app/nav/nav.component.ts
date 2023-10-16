6d97a4ac4d4665c84a1ffd0e5ca932f0fd3f07fc
MPR21 <mikolaj.PLADER.intern@3ds.com>
2023-10-16 19:43:09 +0200

    Logging works, Nav bar created

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit{
+  model: any = {};
+
+  constructor(public accountService: AccountService, private router: Router) {}
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout(){
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
+
+}
