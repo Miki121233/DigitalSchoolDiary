2b92fa214794fd600d48aab95beb1980d276e7f3
MPR21 <mikolaj.PLADER.intern@3ds.com>
2023-11-03 23:28:45 +0100

    HTML-side grades component improvements

--- client/src/app/grades/grades.component.ts
+++ client/src/app/grades/grades.component.ts
@@ -8,6 +8,8 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GradesComponent implements OnInit {
   class: any;
+  description: string = ''; // Dodajemy pole description
+  studentDescriptions: string[] = []; // Dodajemy tablicę do przechowywania opisów za co
 
   constructor(private route: ActivatedRoute) { }
 
@@ -15,10 +17,23 @@ export class GradesComponent implements OnInit {
     this.route.data.subscribe({
       next: data => {
         this.class = data['class'];
-        console.log(data);
+        this.initializeStudentData(); // Wywołujemy funkcję do inicjalizacji danych studentów
       }
-    })
-
+    });
   }
 
+  initializeStudentData() {
+    if (this.class.students) {
+      for (const student of this.class.students) {
+        this.studentDescriptions.push(''); // Dodajemy pustą wartość do tablicy opisów za co
+      }
+    }
+  }
+
+  updateDescriptions() {
+    // Ta funkcja jest wywoływana przy zmianach w polu "Opis"
+    for (let i = 0; i < this.studentDescriptions.length; i++) {
+      this.studentDescriptions[i] = this.description;
+    }
+  }
 }
