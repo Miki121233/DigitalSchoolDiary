3ec57f22e4a84231f2a9a828830b4c8e0274ce46
MPR21 <mikolaj.PLADER.intern@3ds.com>
2023-12-03 16:33:20 +0100

    Loading alike bar for counting days for holidays

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { formatDistanceToNow } from 'date-fns';
+import { pl } from 'date-fns/locale';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,37 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit{
+  percentage: number = 0;
+  daysRemaining?: number | string;
 
   constructor(public accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.howManyDaysRemainingForHolidays();
+  }
+
+  howManyDaysRemainingForHolidays() {
+    const today = new Date();
+    if (today > new Date(today.getFullYear(), 5, 22) && today < new Date(today.getFullYear(), 8, 1)){
+      this.percentage = 100;
+      this.daysRemaining = '0 - teraz są wakacje!'
+    }
+    else {
+      // 21 czerwca - 5, bo zaczynaja sie od zera
+      const nextSummerVacation = new Date(today.getFullYear(), 5, 21);
+      
+      if (today >= nextSummerVacation) {
+        nextSummerVacation.setFullYear(today.getFullYear() + 1);
+      }
+
+      this.daysRemaining = Math.floor((nextSummerVacation.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
+    
+      this.percentage = Math.min((1 - this.daysRemaining / (365-72)) * 100, 100);
+      console.log('this.percentage');
+      console.log(this.percentage);
+      console.log('this.daysRemaining');
+      console.log(this.daysRemaining);
+    }
+    
   }
 }
