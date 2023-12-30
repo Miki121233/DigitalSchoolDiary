dc9fa1c0b075fcabbd55e16f870072d6f16a0300
Mikołaj Pląder <miki.plader@gmail.com>
2023-12-30 14:52:07 +0100

    Minor change

--- API/Dtos/PostHomeworkDto.cs
+++ API/Dtos/PostHomeworkDto.cs
@@ -9,8 +9,6 @@ public class PostHomeworkDto
     public string Comment { get; set; }
     [Required]
     public int TeacherId { get; set; }
-    // DateTime PublishDate { get; set; } = DateTime.UtcNow;
-    //[Required]
     public DateTime Deadline { get; set; }
     [Required]
     public int SubjectId { get; set; }
